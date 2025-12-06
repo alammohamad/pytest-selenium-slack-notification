ce9f658e9fb01059ae2fb16d7626f38394673b53
alammohamad <alambd61@yahoo.com>
2025-12-06 11:59:43 -0500

    Force test failure for Slack CI notification demo

--- tests/test_google_title.py
+++ tests/test_google_title.py
@@ -1,3 +1,5 @@
 def test_google_title(driver):
     driver.get("https://www.google.com")
-    assert "Google" in driver.title
+    #assert "Google" in driver.title
+    ##assert "Google" in driver.title + "FAIL"
+    assert "Bing" in driver.title  # This will fail
